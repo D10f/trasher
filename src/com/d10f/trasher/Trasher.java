8b4a4b6cfee70a9cab02add40501ddf36314dddc
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2025-04-26 07:12:42 -0400

    Refactor entry point to catch mutually exclusive options being used together.

--- src/com/d10f/trasher/Trasher.java
+++ src/com/d10f/trasher/Trasher.java
@@ -1,11 +1,18 @@
 package com.d10f.trasher;
 
 import picocli.CommandLine;
+import picocli.CommandLine.MutuallyExclusiveArgsException;
 import com.d10f.trasher.args.ArgumentParser;
 
 public class Trasher {
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new ArgumentParser()).execute(args);
-        System.exit(exitCode);
+        int exitCode = 0;
+        try {
+            exitCode = new CommandLine(new ArgumentParser()).execute(args);
+        } catch (MutuallyExclusiveArgsException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            System.exit(exitCode);
+        }
     }
 }
